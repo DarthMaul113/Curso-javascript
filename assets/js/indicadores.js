313d430eccaf55c9b5e5efd6b381354dbcf25594
DarthMaul113 <csanchezparilo@gmail.com>
2022-09-27 23:45:06 -0300

    AVANCE FINAL 27-09
    Co-authored-by: Francisco Cox <franciscocox@users.noreply.github.com>

--- assets/js/indicadores.js
+++ assets/js/indicadores.js
@@ -1,26 +1,65 @@
-export function Indicators() {
-    const indicador = () => {
-        try{
-            fetch('https://mindicador.cl/api').parse()
-        } catch (error) {
-            console.log("Requestfailed", error);
-        };
-    }
-
-    return indicador;
+function Indicators() {
+    fetch('https://mindicador.cl/api').then(function (response) {
+        return response.json()
+    }).then(
+        function (indicator) {
+            document.getElementById("UF").value = indicator.uf.valor
+            document.getElementById("Euro").value = indicator.euro.valor
+            document.getElementById("Dolar").value = indicator.dolar.valor
+            document.getElementById("UTM").value = indicator.utm.valor
+        }
+    ).catch(function (error) {
+        console.log("Requestfailed", error)
+    })
 }
 
+var intervalId = window.setInterval(function () {
+    do {
+        var i = 1
+        i++
+        switcher();
+    } while (i<=10)
+}, 5000);
 
-/*
-fetch('https://mindicador.cl/api').then(function (response) {
-        return response.json();
-    }).then(function (dailyIndicators) {
-        dailyIndicators.uf.valor;
-        dailyIndicators.dolar.valor;
-        dailyIndicators.euro.valor;
-        dailyIndicators.utm.valor;
 
-    }).catch(function (error) {
-        console.log("Requestfailed", error);
-    });
-*/
+document.getElementById("selector1").addEventListener("click", switcher)
+
+function switcher() {
+    Indicators()
+    //Captura valor del Selector
+    let valSelector = document.getElementById("selector1").value;
+    let uf = document.getElementById("UF").value
+    let utm = document.getElementById("UTM").value
+    let dolar = document.getElementById("Dolar").value
+    let euro = document.getElementById("Euro").value
+    let valor1 = document.getElementById("valor1").value
+    let valor2 = document.getElementById("valor2").innerHTML
+    let res = 0
+
+
+    switch (valSelector) {
+        case "uf":
+            res = valor1 * uf
+            document.getElementById("valor2").setAttribute('value', res)
+            console.log(valor2)
+            break
+        case "utm":
+            res = valor1 * utm
+            document.getElementById("valor2").setAttribute('value', res)
+            console.log(valor2)
+            break
+        case "dolar":
+            res = valor1 * dolar
+            document.getElementById("valor2").setAttribute('value', res)
+            console.log(valor2)
+            break
+        case "euro":
+            res = valor1 * euro
+            document.getElementById("valor2").setAttribute('value', res)
+            console.log(valor2)
+            break
+        default:
+            res = "error"
+            break
+    }
+}
