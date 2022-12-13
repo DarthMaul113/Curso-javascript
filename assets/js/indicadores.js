dab3cbaa71e439c5460189c28cc2b36461c89e6e
DarthMaul113 <csanchezparilo@gmail.com>
2022-12-13 13:02:47 -0300

    Commit 13-12

--- assets/js/indicadores.js
+++ assets/js/indicadores.js
@@ -1,3 +1,9 @@
+//El boton  debe llamar a historial
+let button = document.querySelector('#historial');
+button.addEventListener('click', () => {
+    historial();
+});
+
 window.onload = Indicators();
 function Indicators() {
     fetch('https://mindicador.cl/api').then(function (response) {
@@ -9,11 +15,14 @@ function Indicators() {
             document.getElementById("Euro").value = indicator.euro.valor
             document.getElementById("Dolar").value = indicator.dolar.valor
             document.getElementById("UTM").value = indicator.utm.valor
+            return indicator
         }
     ).catch(function (error) {
         console.log("Requestfailed", error)
     })
+
 }
+
 
 
 var intervalId = window.setInterval(function () {
@@ -25,7 +34,7 @@ document.getElementById("selector1").addEventListener("click", switcher)
 
 function switcher() {
     Indicators()
-    //Se guarda el valor capturado en las variables correspondientes
+    //Se guarda el valor capturado en las variables correspondientes y se declaran variables
     let valSelector = document.getElementById("selector1").value;
     let uf = document.getElementById("UF").value
     let utm = document.getElementById("UTM").value
@@ -40,22 +49,18 @@ function switcher() {
         case "uf":
             res = valor1 * uf
             document.getElementById("valor2").setAttribute('value', Math.trunc(res).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","))
-            console.log(valor2)
             break
         case "utm":
             res = valor1 * utm
             document.getElementById("valor2").setAttribute('value', Math.trunc(res).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","))
-            console.log(valor2)
             break
         case "dolar":
             res = valor1 * dolar
             document.getElementById("valor2").setAttribute('value', Math.trunc(res).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","))
-            console.log(valor2)
             break
         case "euro":
             res = valor1 * euro
             document.getElementById("valor2").setAttribute('value', Math.trunc(res).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","))
-            console.log(valor2)
             break
         default:
             res = "error"
@@ -63,19 +68,61 @@ function switcher() {
     }
 }
 
-
 function historial() {
     Indicators()
-    console.log(document.getElementById("UF").value);
-
     const fecha = new Date();
 
+    //Declaracion de variables
+    let uf = document.getElementById("UF").value
+    let utm = document.getElementById("UTM").value
+    let dolar = document.getElementById("Dolar").value
+    let euro = document.getElementById("Euro").value
     let hoy = `${fecha.getDate()}-${fecha.getMonth() + 1}-${fecha.getFullYear()}`;
     let hora = `${fecha.getHours()}:${(fecha.getMinutes() < 10 ? '0' : '') + fecha.getMinutes()}`;
     let tipo;
     let valor;
     let registroHistorial = [];
 
+    //Crear tabla donde se almacenan los datos
+    const crearVal = () => {
+        let valTablaDiv = document.querySelector("div.tablaHistorialDiv")
+        let table = document.querySelector("table.tablaHistorial")
+        let tableHead = document.querySelector("thead.tablaHistorialHead")
+
+        //Ciclo para recorrer el largo del arreglo tiposInd
+        let index = 0
+        tiposInd.forEach(element => {
+            index = index + 1
+
+            //Declarar variables y DOM
+            let valTablaRow = document.createElement("tr")
+            valTablaRow.className = "TablaRow"
+            let valTipo = document.createElement("th")
+            valTipo.className = "valTipo"
+            let valValor = document.createElement("th")
+            valValor.className = "valValor"
+            let valDia = document.createElement("th")
+            valDia.className = "valDia"
+            let valHora = document.createElement("th")
+            valHora.className = "valHora"
+
+            //Llamada a los objetos que almacenan los datos
+            valTipo.innerText = registroHistorial[index].tipo
+            valValor.innerText = registroHistorial[index].valor
+            valDia.innerText = registroHistorial[index].fecha
+            valHora.innerText = registroHistorial[index].hora
+
+            //Se adjuntan las variables correspondientes a cada parte de la tabla
+            valTablaRow.append(valTipo)
+            valTablaRow.append(valValor)
+            valTablaRow.append(valDia)
+            valTablaRow.append(valHora)
+            tableHead.append(valTablaRow)
+            table.append(tableHead)
+            valTablaDiv.append(table)
+        });
+    }
+    //Se crea el constructor
     class registro {
         constructor(fecha, hora, tipo, valor) {
             this.fecha = fecha;
@@ -85,38 +132,33 @@ function historial() {
         }
     }
 
+    //Arreglo y recorrido
     const tiposInd = ["UF", "UTM", "DOLAR", "EURO"];
     for (let index = 0; index < tiposInd.length; index++) {
-        console.log(tiposInd[index])
+
+        //Usando la iteracion de recorrido de usa un switch para obtener datos de cada tipo de indicador
         switch (tiposInd[index]) {
             case "UF":
                 tipo = "UF"
-                valor = document.getElementById("UF").value
+                valor = uf
                 registroHistorial[1] = new registro(hoy, hora, tipo, valor);
             case "UTM":
                 tipo = "UTM"
-                valor = document.getElementById("UTM").value
+                valor = utm
                 registroHistorial[2] = new registro(hoy, hora, tipo, valor);
             case "DOLAR":
                 tipo = "DOLAR"
-                valor = document.getElementById("Dolar").value
+                valor = dolar
                 registroHistorial[3] = new registro(hoy, hora, tipo, valor);
-
             case "EURO":
                 tipo = "EURO"
-                valor = document.getElementById("Euro").value
+                valor = euro
                 registroHistorial[4] = new registro(hoy, hora, tipo, valor);
             default:
                 tipo = "error"
                 break
         }
-
     }
-    console.log(registroHistorial)
-
-
+    //Se llama a la tabla
+    crearVal()
 }
-console.log(historial())
-
-
-
